16e263b127b2ba7f076be480699366b53ef6ebac
anshumanpbh@gmail.com <anshumanpbh@gmail.com>
2020-05-01 20:18:24 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	type placeholder [5]string
+
+	zero := placeholder{
+		"███",
+		"█ █",
+		"█ █",
+		"█ █",
+		"███",
+	}
+
+	one := placeholder{
+		"██ ",
+		" █ ",
+		" █ ",
+		" █ ",
+		"███",
+	}
+
+	two := placeholder{
+		"███",
+		"  █",
+		"███",
+		"█  ",
+		"███",
+	}
+
+	three := placeholder{
+		"███",
+		"  █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	four := placeholder{
+		"█ █",
+		"█ █",
+		"███",
+		"  █",
+		"  █",
+	}
+
+	five := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"  █",
+		"███",
+	}
+
+	six := placeholder{
+		"███",
+		"█  ",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	seven := placeholder{
+		"███",
+		"  █",
+		"  █",
+		"  █",
+		"  █",
+	}
+
+	eight := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"█ █",
+		"███",
+	}
+
+	nine := placeholder{
+		"███",
+		"█ █",
+		"███",
+		"  █",
+		"███",
+	}
+
+	digits := [...]placeholder	{
+		zero,
+		one,
+		two,
+		three,
+		four,
+		five,
+		six,
+		seven,
+		eight,
+		nine,
+	}
+
+
+
+	for i := 0 ; i <5 ; i++ {
+
+		for i2  := range digits {
+			fmt.Print(digits[i2][i],"  ")
+
+		}
+
+		fmt.Println()
+
+	}
+
+}
